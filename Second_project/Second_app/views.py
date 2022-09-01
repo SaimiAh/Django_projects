f9252ff5a8fdf3c712ab9119d0dbba79a1cfa7c6
SaimiAh <ah.saim786@gmail.com>
2022-09-01 22:17:56 +0500

    Modified my Second commit of the day.

--- Second_project/Second_app/views.py
+++ Second_project/Second_app/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     mydic = {'insert_me': "Hi this Is my page!"}
-    return render(request,'index.html',context=mydic)
+    return render(request,'Second_app/index.html',context=mydic)
 
 # Create your views here.
