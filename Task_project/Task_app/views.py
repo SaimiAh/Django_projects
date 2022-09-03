5eaf7235d15e895ed9de3ac38e5a909910b58c5c
SaimiAh <ah.saim786@gmail.com>
2022-09-03 20:11:20 +0500

    Modified my Second commit of the day.

--- Task_project/Task_app/views.py
+++ Task_project/Task_app/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello I am in views")
+    return HttpResponse("This is from the view.py file")
+
+def help(request):
+    helpdic = {'help_insert':'Help Page'}
+    return render(request,'Task_app/help.html',context=helpdic)
 
 def help(request):
     helpdic = {'help_insert':'Help Page'}
